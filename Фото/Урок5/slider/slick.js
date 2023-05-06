abd47928b03a428a06e7012ffca90e9dc1dcefca
YummY <117013234+DecorativeCactus@users.noreply.github.com>
2023-05-06 18:28:20 +0300

    Add files via upload

---
+++ Фото/Урок5/slider/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.content__slider').slick({
+        dots:true
+    });
+    $('.posts__slider').slick({
+        
+        slidesToShow:3,
+        centerPadding: '30px',
+        centerMode:true,
+    });
+});
